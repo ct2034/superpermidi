f11910e6caa7eea92172551f30835901af6d2b73
Christian Henkel <post@henkelchristian.de>
2019-03-18 11:21:04 +0100

    cleaner doc

--- superpermidi.py
+++ superpermidi.py
@@ -1,32 +1,28 @@
 from midiutil.MidiFile import MIDIFile, SHARPS, MAJOR
 import sys
 
-# source / writing midi file:
-# https://stackoverflow.com/questions/11059801/how-can-i-write-a-midi-file-with-python
 fname = sys.argv[1]
 fname_no_ext = fname.split('.')[0]
 
-# create your MIDI object
-mf = MIDIFile(1)     # only 1 track
-track = 0   # the only track
-
-time = 0    # start at the beginning
+# source / writing midi file:
+# https://stackoverflow.com/questions/11059801/how-can-i-write-a-midi-file-with-python
+mf = MIDIFile(1)
+track = 0
+time = 0
 mf.addTrackName(track, time, fname_no_ext)
 mf.addTempo(track, time, 120)
 mf.addKeySignature(track, time, 0, SHARPS, MAJOR)
-
-# add some notes
 channel = 0
 volume = 100
 
-lookup = {  # note numbers: https://newt.phys.unsw.edu.au/jw/notes.html
-    '1': 60, # C4
-    '2': 62, # D
-    '3': 64, # E
-    '4': 65, # F
-    '5': 67, # G
-    '6': 69, # A
-    '7': 71  # B
+lookup = {  # ref note numbers: https://newt.phys.unsw.edu.au/jw/notes.html
+    '1': 60,  # C4
+    '2': 62,  # D
+    '3': 64,  # E
+    '4': 65,  # F
+    '5': 67,  # G
+    '6': 69,  # A
+    '7': 71   # B
 }
 
 with open(fname, 'r') as f:
@@ -34,8 +30,8 @@ with open(fname, 'r') as f:
         if not l.startswith("#"):
             for i, n in enumerate(l):
                 try:
-                    pitch = lookup[n]    # C4 (middle C)
-                    time = i/2           # start on beat 0
+                    pitch = lookup[n]
+                    time = i/2           # two per beat
                     duration = .5        # half note
                     mf.addNote(track, channel, pitch, time, duration, volume)
                 except KeyError:
@@ -43,6 +39,5 @@ with open(fname, 'r') as f:
                         print("KeyError: " + n)
                     pass
 
-# write it to disk
 with open(fname_no_ext+".mid", 'wb') as outf:
     mf.writeFile(outf)
